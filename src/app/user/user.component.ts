4f699f0cc51a45e54e879796a34981459e4a11bf
anhnguyen <anh310194@gmail.com>
2020-12-09 17:11:38 +0700

    up

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,19 +11,18 @@ import { UserService } from '../_services/user.service'
 })
 export class UserComponent implements OnInit {
   tableReponse!: TableReponse<UserManagementResponse>;
-  isHidden: boolean;
+  isHidden: boolean = false;
 
   constructor(private userService: UserService) {
-    this.isHidden = false;
     this.tableReponse = new TableReponse<UserManagementResponse>();
-  }
-
-  ngOnInit(): void {
     this.LoadBodyTable();
   }
 
+  ngOnInit(): void {
+  }
+
   LoadBodyTable() {
-    this.isHidden =true;
+    this.isHidden = true;
     this.userService.GetUserList().pipe(first())
       .subscribe(
         data => {
